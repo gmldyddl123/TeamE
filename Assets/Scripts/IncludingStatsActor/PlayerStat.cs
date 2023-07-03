7ca6306452c047641d7bb0539a61e8a8867690d6
basasac <vox325@naver.com>
2023-07-03 17:04:10 +0900

    캐릭 변경 기반 강화

--- Assets/Scripts/IncludingStatsActor/PlayerStat.cs
+++ Assets/Scripts/IncludingStatsActor/PlayerStat.cs
@@ -11,34 +11,18 @@ public class PlayerStat : IncludingStatsActor
     public CapsuleCollider attackCollider;
 
     //Vector3 moveTargetDir;
-    float attackForwardMoveSpeed = 5.0f;
+    
 
 
 
 
     private void Awake()
     {
-        playerInputSystem = GetComponent<PlayerInputSystem>();
-        characterController = GetComponent<CharacterController>();
+        //GetComponentInParent<PlayerInputSystem>();
+        playerInputSystem = GetComponentInParent<PlayerInputSystem>();
+        characterController = GetComponentInParent<CharacterController>();
     }
 
-
-
-    public override void Attack(Vector3 movedir)
-    {
-        //근거리는 앞으로
-        //원거리는 뒤로
-
-        Debug.Log("델리게이트 어택");
-
-        playerInputSystem.UseGravity();
-        //적한테 살짝 접근 attackMove 값은 애니메이션 이밴트에서 실행된다
-        if (playerInputSystem.attackMove)
-        {
-            //playerInputSystem.UseGravity();
-            characterController.Move(attackForwardMoveSpeed * Time.fixedDeltaTime * movedir);
-        }
-    }
 
 
     public void AttackColliderActive()
