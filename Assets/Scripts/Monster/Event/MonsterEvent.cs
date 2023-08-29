ec12028610eb91fc79ea98960a8e8e1dfe8fe0af
gmldyddl123 <gmldyddl123@gmail.com>
2023-08-29 15:18:47 +0900

    func란?

--- Assets/Scripts/Monster/Event/MonsterEvent.cs
+++ Assets/Scripts/Monster/Event/MonsterEvent.cs
@@ -8,9 +8,19 @@ public class MonsterEvent : MonoBehaviour
     public Action<Monster> OnMonsterAttacked;
     public Action SpawnCountChange;
 
+    public Func<int, bool> test;
+    private void Start()
+    {
+        test += TEST1;
+    }
 
     public void MonsterAttacked(Monster attackedMonster)
     {
         OnMonsterAttacked?.Invoke(attackedMonster);
+    }
+
+    bool TEST1(int test)
+    {
+        return true;
     }
 }
