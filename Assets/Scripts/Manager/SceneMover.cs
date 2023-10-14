d7a5740cd53e93c21036dbb162d9f8bad5734244
gmldyddl123 <119833234+gmldyddl123@users.noreply.github.com>
2023-10-15 00:32:04 +0900

    씬이동 스크립트 추가, 이동위치 추가

--- Assets/Scripts/Manager/SceneMover.cs
+++ Assets/Scripts/Manager/SceneMover.cs
@@ -1,3 +1,4 @@
+using player;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneMover : MonoBehaviour
 {
+    PlayerController player;
+    public GameObject genPos;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
     public void BossSceneEnter()
     {
+        player.transform.position = genPos.transform.position;
         SceneManager.LoadScene("Boss_Scene");
     }
 }
